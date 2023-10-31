e84ea8ab9da5d613b17bafb204ef33ac742a21a6
alkathirikhalid <khalid@alkathirikhalid.com>
2023-10-31 11:24:24 +0800

    Added Them, dims, color, views and recycle view

--- app/src/main/java/com/alkathirikhalid/androidroomwithaview/MainActivity.kt
+++ app/src/main/java/com/alkathirikhalid/androidroomwithaview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.alkathirikhalid.androidroomwithaview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // RV
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
