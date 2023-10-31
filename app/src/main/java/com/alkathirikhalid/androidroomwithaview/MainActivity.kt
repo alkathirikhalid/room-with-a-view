2814e84be12cd12dd1ce223e02a068fa71d64545
alkathirikhalid <khalid@alkathirikhalid.com>
2023-10-31 13:56:11 +0800

    Updated Main Screen to handle add and view db live data

--- app/src/main/java/com/alkathirikhalid/androidroomwithaview/MainActivity.kt
+++ app/src/main/java/com/alkathirikhalid/androidroomwithaview/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.alkathirikhalid.androidroomwithaview
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityResultLauncher: ActivityResultLauncher<Intent>
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +27,37 @@ class MainActivity : AppCompatActivity() {
         val adapter = WordListAdapter()
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        wordViewModel.allWords.observe(this) { words ->
+            // Update the cached copy of the words in the adapter.
+            words.let { adapter.submitList(it) }
+        }
+        // Handle Floating Action Bar
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            mainActivityResultLauncher.launch(intent)
+        }
+        // Initialize the ActivityResultLauncher
+        mainActivityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            // Handle the result in onActivityResult
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let { reply ->
+                    val word = Word(reply)
+                    wordViewModel.insert(word)
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
